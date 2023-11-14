fc9fb376e11c3a68ba3a99a4d99b9352cb060a7c
karthigaalagesan <131602040+karthigaalagesan@users.noreply.github.com>
2023-11-14 06:21:06 -0800

    Delete Backend directory

--- Backend/src/models/order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-  totalPrice: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('Order', orderSchema);
